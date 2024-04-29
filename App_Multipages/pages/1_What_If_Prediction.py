f98563799c8fad2f587371c4cd891ce2544b9d80
Mateus Schualtz <mateus.schualtz@quiteja.com.br>
2024-04-29 09:39:05 -0300

    Mensagem de commit

---
+++ App_Multipages/pages/1_What_If_Prediction.py
@@ -0,0 +1,40 @@
+import pickle
+import pandas as pd
+import streamlit as st
+import sklearn
+
+#Page Config
+st.set_page_config(page_title='Insurance Prediction')
+st.sidebar.header('What if Prediction')
+st.title("Insurance prediction")
+
+st.markdown("Predict medical insurance based on the following features:")
+
+#-- Parameters --#
+
+age = st.number_input(label='Age', value=18, min_value=18, max_value=120)
+bmi = st.number_input(label='BMI', value=30.)
+children = st.slider(label='Children', min_value=0, max_value=5)
+smoker = st.selectbox(label='Smoker', options=['no','yes'])
+
+# -- Model -- #
+
+with open('../models/model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+def prediction():
+    df_input = pd.DataFrame([{
+        'age': age,
+        'bmi': bmi,
+        'children': children,
+        'smoker': smoker
+    }])
+    prediction = model.predict(df_input)[0]
+    return prediction
+
+if st.button('Predict'):
+    try:
+        insurance_cost = prediction()
+        st.success(f'**Predicted insurance price:** ${insurance_cost:,.2f}')
+    except Exception as error:
+        st.error(f"Couldn't predict the input data. The following error occurred: \n\n{error}")
---
+++ App_Multipages/pages/2_File_Prediction.py
@@ -0,0 +1,31 @@
+import pickle
+import pandas as pd
+import streamlit as st
+import sklearn
+
+#Page Config
+st.set_page_config(page_title='Insurance Prediction')
+st.sidebar.header('File Prediction')
+st.title("Insurance prediction")
+
+st.markdown("Predict medical insurance based on the using csv file:")
+
+# -- Model -- #
+
+with open('../models/model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+data = st.file_uploader('Upload your file')
+
+if data:
+    df_input = pd.read_csv(data)
+    insurance_prediction = model.predict(df_input)
+    df_output = df_input.assign(prediction = insurance_prediction)
+
+    st.markdown('Insurance Cost Prediction')
+    st.write(df_output)
+    st.download_button(
+        label='Download CSV', 
+        data=df_output.to_csv(index=False).encode('utf-8'), 
+        mime='text/csv',
+        file_name= 'Prediction_Insurance_Cost.csv')
